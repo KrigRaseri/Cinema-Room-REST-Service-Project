5c0469892772ea61b417e2107dbe2f34d92778b6
Krig Raseri <krigraseri@gmail.com>
2023-07-14 06:47:53 -0500

    Update CinemaControllerTest.java

--- src/test/java/com/umbrella/cinemarestservice/presentation/CinemaControllerTest.java
+++ src/test/java/com/umbrella/cinemarestservice/presentation/CinemaControllerTest.java
@@ -3,7 +3,7 @@ package com.umbrella.cinemarestservice.presentation;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -12,7 +12,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@WebMvcTest(CinemaController.class)
 @AutoConfigureMockMvc
 class CinemaControllerTest {
 
