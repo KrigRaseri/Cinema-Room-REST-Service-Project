8805abe354bad8590c2edac1c324a508c5da4ab6
Krig Raseri <krigraseri@gmail.com>
2023-10-04 03:15:14 -0500

    Chore: change CustomErrorResponse to a record
    Changes CustomErrorResponse to a record to simplify the class and remove the @NonNull annotations.

--- src/main/java/com/umbrella/cinemarestservice/exceptionhandling/CustomErrorResponse.java
+++ src/main/java/com/umbrella/cinemarestservice/exceptionhandling/CustomErrorResponse.java
@@ -1,25 +1,8 @@
 package com.umbrella.cinemarestservice.exceptionhandling;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.Getter;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-
-import java.time.LocalDateTime;
-
-@Getter
-@Setter
-@RequiredArgsConstructor
-public class CustomErrorResponse {
-
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss")
-    private LocalDateTime timestamp = LocalDateTime.now();
-
-    @NonNull
-    private int status;
-
-    @NonNull
-    private String error;
+/**
+ * Custom error response to format the error message.
+ */
+public record CustomErrorResponse(int status, String error) {
 
 }
